cf8f0d5cebedfd27943a188760abb7897dccaca6
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-02-13 21:17:21 +0300

    add transaction

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,6 +1,7 @@
 import json
 import phonenumbers
 
+from django.db import transaction
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
@@ -101,6 +102,7 @@ def product_list_api(request):
     })
 
 
+@transaction.atomic
 @api_view(['POST'])
 def register_order(request):
     serializer = OrderSerializer(data=request.data)

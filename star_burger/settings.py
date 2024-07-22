5014bb0dafffa6c5071b6b6af92fe9ff4405bfc4
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-07-22 22:11:18 +0300

    Update settings.py

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -96,12 +96,6 @@ DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-#     )
-# }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
